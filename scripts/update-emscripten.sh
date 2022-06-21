b0b1c614bf0bb74c7439789b1dff99e1a8fc00fc
Isaac Brodsky <isaac@unfolded.ai>
2022-06-21 15:52:20 -0400

    rename in library

--- scripts/update-emscripten.sh
+++ scripts/update-emscripten.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2018-2019 Uber Technologies, Inc.
+# Copyright 2018-2019, 2022 Uber Technologies, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
