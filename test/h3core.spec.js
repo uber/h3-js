0a7cefcb70baeedd675d47b8c54bcdce8d4249e8
Isaac Brodsky <isaac@isaacbrodsky.com>
2025-11-14 17:50:51 -0800

    prettier

--- test/h3core.spec.js
+++ test/h3core.spec.js
@@ -1609,27 +1609,11 @@ test('getIndexDigit', assert => {
 test('constructCell', assert => {
     assert.throws(
         () =>
-            h3.constructCell(0, [
+            h3.constructCell(
                 0,
-                1,
-                2,
-                3,
-                4,
-                5,
-                6,
-                7,
-                8,
-                9,
-                10,
-                11,
-                12,
-                13,
-                14,
-                15,
-                16,
-                17,
-                18
-            ], 0),
+                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
+                0
+            ),
         {code: E_DIGIT_DOMAIN},
         'invalid (bad digit array)'
     );
